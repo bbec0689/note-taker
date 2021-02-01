f5dfe3141174993734b564348ee71b1f379ca056
Dave <bbec0689@gmail.com>
2021-02-01 15:34:39 -0700

    updating file structure, updating route

---
+++ routes/apiRoutes/apiRoute.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const db = require("../../db/db.json");
+let id = 0;
+
+router.get("/notes", (req, res) => {
+  res.json(db);
+  console.log(db);
+});
+
+router.post("/notes", (req, res) => {
+
+    console.log(db);
+  });
+
+  async function mid(req, res, next) {
+
+    id = id + 1;
+    let obj = { id: id }
+    db.push({ ...obj, ...req.body });
+    res.json(db);
+    next();
+
+
+  }
+
+  module.exports = router;
